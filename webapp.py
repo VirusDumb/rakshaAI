b026a979ae6249147514d6a212e70cf608784711
VirusDumb <soumyaray532@gmail.com>
2025-09-20 16:43:29 +0530

    ok

--- webapp.py
+++ webapp.py
@@ -1,2 +1,13 @@
 import streamlit as st
 from wpmain import phishingtextagent
+from agno.agent import Agent, RunResponse
+st.title("Raksha AI")
+mail=st.text_area("Email text",height=200)
+if st.button("Check Phising Probability"):
+    with st.spinner(text="Evaluating...."):
+        try:
+            response: RunResponse = phishingtextagent.run(mail)
+            st.write("**Evaluation**")
+            st.write(response.content)
+        except Exception as e:
+            st.error(f"error: {e}")
--- wpmain.py
+++ wpmain.py
@@ -16,7 +16,7 @@ from typing import Any, Callable, Dict
 #from os import getenv
 from agno.team import Team
 from agno.tools import tool
-colablink='https://c2d1714d0e5e.ngrok-free.app/analyze'
+colablink='https://nonprojecting-tamar-hysterically.ngrok-free.app/analyze'
 #ollhost='https://64e24cff7d4e.ngrok-free.app/'
 import requests
 import requests
@@ -57,7 +57,7 @@ phishingtextagent=Agent(agent_id="PTA",
     model=Groq(id="openai/gpt-oss-20b"),
     instructions=("""
                   You are an phishing protection agent your task is to read the message user sends and predict if it's a phishing mail \n
-                  Run through Evaluator Model using evaluatormodel tool to get a % of classification of phishing probability
+                  Run through Evaluator Model using evaluatormodel tool to get a % of classification of phishing probability and put that in the final output's top\n
                   Read the text carefully and tell the user why it might be a scam or phishing attack, use google search and duckduckgo search to verify any added links and use website tools to check for inconsistency in the website \n
                   """),
     tools=[WhatsAppTools(),GoogleSearchTools(),WikipediaTools(),WebsiteTools(),evaluatormodel],
@@ -73,7 +73,7 @@ webhtmlagent=Agent(agent_id="WSA",
     tools=[WhatsAppTools(),GoogleSearchTools(),WikipediaTools(),WebsiteTools()],
     debug_mode=True)
 
-#phishingtextagent.print_response("URGENT: YOUR ACCOUNT HAS BEEN SUSPENDED DUE TO SUSPICIOUS ACTVITY, CLICK LINK TO KNOW MORE https://sbibank.xyz")
+phishingtextagent.print_response("URGENT: YOUR ACCOUNT HAS BEEN SUSPENDED DUE TO SUSPICIOUS ACTVITY, CLICK LINK TO KNOW MORE https://sbibank.xyz")
 
 fstapp=FastAPIApp(agents=[phishingtextagent])
 wpapp=WhatsappAPI(agent=phishingtextagent)
